108ae9a2723053ae740a8eb6f22a9694c13a95f0
wdf <wdf332@gmail.com>
2023-01-07 01:31:28 +0800

    commit

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,13 +14,10 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    themes: {
-      light: {
-        colors: {
-          primary: '#F06292',
-          secondary: '#F8BBD0'
-        }
-      }
-    }
+    defaultTheme: 'light'
+  },
+  colors: {
+    primary: '#F06292',
+    secondary: '#F8BBD0'
   }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,39 @@
 <template lang="pug">
 v-row#home.text-center
-  v-col(cols="12")
+  v-col.v-col-lg-6.v-col-12
     h1 {{ currentText }}
-    h1 {{ currentTime }}
+    h1 剩餘 {{ currentTime }}
 v-row.text-center
-  v-col(cols="12")
-    v-btn(v-if="status !== 1" icon="mdi-play" variant="text" @click="startTimer")
-    v-btn(v-if="status === 1" icon="mdi-pause" variant="text" @click="pauseTimer")
-    v-btn(v-if="currentItem.length >= 0" icon="mdi-skip-next" variant="text" @click="finishTimer")
+  v-col.v-col-lg-6.v-col-12(cols="6")
+    v-btn(v-if="status !== 1" icon="mdi-play" variant="text" @click="startTimer" size="96")
+    v-btn(v-if="status === 1" icon="mdi-pause" variant="text" @click="pauseTimer" size="96")
+    v-btn(v-if="currentItem.length >= 0" icon="mdi-skip-next" variant="text" @click="finishTimer" size="96")
+v-row
+  v-col.v-col-lg-6.v-col-12(cols="6" justify="center")
+    h1#thing.text-center 待辦事項
+    v-text-field(ref="input" v-model="newItem" label="新增事項" :rules="[rules.required, rules.length]" @keydown.enter="onInputSubmit")
+      template(#append)
+        v-btn(icon="mdi-plus" variant="text" @click="onInputSubmit")
+  v-col.v-col-12.v-col-lg-6(align="end")
+    v-table
+      thead
+        tr
+          th 名稱
+          th 操作
+      tbody
+        tr(v-if="items.length === 0")
+          td.text-center(colspan="2") 沒有事項
+        tr(v-for="item in items" v-else :key="item.id" ref="editInputs")
+          td
+            v-text-field(v-if="item.edit" v-model="item.model" autofocus :rules="[rules.required, rules.length]")
+            span(v-else) {{ item.name }}
+          td
+            span(v-if="item.edit")
+              v-btn(icon="mdi-check" variant="text" color="green" @click="confirmEditItem(item.id)")
+              v-btn(icon="mdi-undo" variant="text" color="red" @click="undoEditItem(item.id)")
+            span(v-else)
+              v-btn(icon="mdi-pencil" variant="text" color="green" @click="editItem(item.id)")
+              v-btn(icon="mdi-delete" variant="text" color="red" @click="delItem(item.id)")
 </template>
 
 <script setup>
@@ -17,12 +43,33 @@ import { useSettingsStore } from '@/stores/settings'
 import { storeToRefs } from 'pinia'
 
 const list = useListStore()
-const { currentItem, items, timeleft } = storeToRefs(list)
-const { start, countdown, finish } = list
+const { currentItem, items, timeleft, finishedItems } = storeToRefs(list)
+const { start, countdown, finish, addItem, editItem, delItem, confirmEditItem, undoEditItem, delFinishedItem } = list
 
 const settings = useSettingsStore()
 const { selectedAlarmFile, notify } = storeToRefs(settings)
 
+const newItem = ref('')
+const input = ref(null)
+const editInputs = ref([])
+
+const rules = {
+  required (v) {
+    return !!v || '欄位必填'
+  },
+  length (v) {
+    return v.length >= 3 || '必須三個字以上'
+  }
+}
+
+const onInputSubmit = async () => {
+  const valid = await input.value.validate()
+  console.log(valid)
+  if (valid.length > 0) return
+  addItem(newItem.value)
+  input.value.$el.querySelector('input').blur()
+  input.value.reset()
+}
 // 0 = 停止
 // 1 = 倒數中
 // 2 = 暫停
@@ -57,7 +104,7 @@ const finishTimer = () => {
     // eslint-disable-next-line
     const notification = new Notification('事項完成', {
       body: currentText.value + '已完成',
-      icon: 'https://github.com/wdaweb.png'
+      icon: 'https://github.com/wdf3321.png'
     })
   }
   finish()
@@ -67,7 +114,7 @@ const finishTimer = () => {
 }
 
 const currentText = computed(() => {
-  return currentItem.value.length > 0 ? currentItem.value : items.value.length > 0 ? `即將開始 : ${list.items[0].name} ` : '沒有事項'
+  return currentItem.value.length > 0 ? currentItem.value : items.value.length > 0 ? `即將開始 : ${list.items[0].name} ` : '請新增事項'
 })
 const currentTime = computed(() => {
   const m = Math.floor(timeleft.value / 60).toString().padStart(2, '0')
@@ -75,7 +122,7 @@ const currentTime = computed(() => {
   return m + ':' + s
 })
 </script>
-<style>
+<!-- <style>
 .v-main{
   background:#F8BBD0;
 }
@@ -86,4 +133,4 @@ display: flex;
 justify-content: center;
 align-items: center;
 }
-</style>
+</style> -->
